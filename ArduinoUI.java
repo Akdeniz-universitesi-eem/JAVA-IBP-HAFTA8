7e81741c2c4bdb68a41e79464e63559445aad924
ByTheEfsane <59279876+oomerty@users.noreply.github.com>
2023-12-17 22:40:23 +0300

    5 led application completed

---
+++ ArduinoUI.java
@@ -0,0 +1,87 @@
+import com.fazecast.jSerialComm.SerialPort;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ArduinoUI {
+    private static String portName = "COM1";
+    private static JLabel lastChanges = new JLabel("deneme");
+    private static JButton led1BTN = new JButton("LED 1");
+    private static JButton led2BTN = new JButton("LED 2");
+    private static JButton led3BTN = new JButton("LED 3");
+    private static JButton led4BTN = new JButton("LED 4");
+    private static JButton led5BTN = new JButton("LED 5");
+    private static JButton exitBTN = new JButton("Çık");
+
+    public static void arduinoUI() {
+        SerialPort serialPort = SerialPort.getCommPort(portName);
+        serialPort.setComPortParameters(9600, 8, 1, 0); // Baudrate: 9600, Data bits: 8, Stop bits: 1, Parity: None
+        serialPort.openPort(); //
+
+        JFrame appScreen = new JFrame("Arduino LED Kontrol Uygulaması");
+
+        led1BTN.setBounds(100, 25, 200, 24);
+        led2BTN.setBounds(100, 57, 200, 24);
+        led3BTN.setBounds(100, 89, 200, 24);
+        led4BTN.setBounds(100, 121, 200, 24);
+        led5BTN.setBounds(100, 153, 200, 24);
+        lastChanges.setBounds(100, 300, 200, 24);
+        exitBTN.setBounds(100, 325, 200, 24);
+
+        appScreen.add(led1BTN);
+        appScreen.add(led2BTN);
+        appScreen.add(led3BTN);
+        appScreen.add(led4BTN);
+        appScreen.add(led5BTN);
+        appScreen.add(lastChanges);
+        appScreen.add(exitBTN);
+
+        led1BTN.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                String ledNum = "1";
+                lastChanges.setText("LED 1 durumu değiştirildi.");
+                serialPort.writeBytes(ledNum.getBytes(), ledNum.length());
+            }
+        });
+        led2BTN.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                String ledNum = "2";
+                lastChanges.setText("LED 2 durumu değiştirildi.");
+                serialPort.writeBytes(ledNum.getBytes(), ledNum.length());
+            }
+        });
+        led3BTN.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                String ledNum = "3";
+                lastChanges.setText("LED 3 durumu değiştirildi.");
+                serialPort.writeBytes(ledNum.getBytes(), ledNum.length());
+            }
+        });
+        led4BTN.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                String ledNum = "4";
+                lastChanges.setText("LED 4 durumu değiştirildi.");
+                serialPort.writeBytes(ledNum.getBytes(), ledNum.length());
+            }
+        });
+        led5BTN.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                String ledNum = "5";
+                lastChanges.setText("LED 5 durumu değiştirildi.");
+                serialPort.writeBytes(ledNum.getBytes(), ledNum.length());
+            }
+        });
+
+        appScreen.setSize(400,400);
+        appScreen.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        appScreen.setLayout(null);
+        appScreen.setVisible(true);
+
+        exitBTN.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                serialPort.closePort();
+                System.exit(0);
+            }
+        });
+    }
+}
---
+++ out/production/JAVA-IBP-HAFTA8/README.md
@@ -0,0 +1,1 @@
+# JAVA-IBP-HAFTA8
