5fd846b1223f33c40a5e580afa7ffb4d581be646
ByTheEfsane <59279876+oomerty@users.noreply.github.com>
2023-12-18 01:49:20 +0300

    progress on reading data problem
    next step is to display incoming data

--- GraphUI.java
+++ GraphUI.java
@@ -1,3 +1,54 @@
-public class GraphUI {
-    
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
+
+class GraphUI {
+    private static StringBuilder buffer = new StringBuilder();
+
+    public static void grapUI() {
+        String portName = "COM1";
+        SerialPort serialPort = SerialPort.getCommPort(portName);
+        serialPort.setBaudRate(9600);
+        serialPort.openPort();
+
+        if (serialPort.openPort()) {
+            System.out.println("Porta bağlantı sağlandı...");
+
+            serialPort.addDataListener(new SerialPortDataListener() {
+                @Override
+                public int getListeningEvents() {
+                    return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
+                }
+
+                @Override
+                public void serialEvent(SerialPortEvent event) {
+                    if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE) return;
+
+                    byte[] newData = new byte[serialPort.bytesAvailable()];
+                    serialPort.readBytes(newData, newData.length);
+
+                    for (byte b : newData) {
+                        char c = (char) b;
+                        if (c == '\n' || c == '\r') {
+                            processBuffer();
+                            buffer.setLength(0);
+                        } else buffer.append(c);
+                    }
+                }
+            });
+
+            try {
+                Thread.sleep(30000); // İşlem devam ederken bekle
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            serialPort.closePort();
+        } else System.out.println("Porta bağlantı başarısız!");
+    }
+
+    private static void processBuffer() {
+        String receivedData = buffer.toString().trim();
+        if (!receivedData.isEmpty()) System.out.println("Arduino'dan alınan veri: " + receivedData);
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        ArduinoUI.arduinoUI();
+        //ArduinoUI.arduinoUI();
+        GraphUI.grapUI();
     }
 }
